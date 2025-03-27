78722d4999623a433c32d864b73cf9b4c3b91805
Zannatul Adon Sabiha <166790186+ZAsabiha@users.noreply.github.com>
2025-03-28 03:18:08 +0600

    refactoring the RecipeApp class

---
+++ DisplayTtile.java
@@ -0,0 +1,17 @@
+class DisplayTitle{
+    public void display() {
+      System.out.println("\n--- Savour the Flavours of Health ---");
+        System.out.println("1. Add Recipe");
+        System.out.println("2. Display Recipes");
+        System.out.println("3. Meal Planning");
+        System.out.println("4. Dietary Analysis");
+        System.out.println("5. Nutrition Profile");
+        System.out.println("6. Get Food Recommendations for Health Conditions");
+        System.out.println("7. Mood-Based Recommendations");
+        System.out.println("8. Food Waste Tracker");
+        System.out.println("9. Recipe Period Suggestions");
+        System.out.println("10. Calculate BMI and Assess Health");
+        System.out.println("11. Suggest Daily Caloric Intake");
+        System.out.println("0. Exit");
+    }
+}
